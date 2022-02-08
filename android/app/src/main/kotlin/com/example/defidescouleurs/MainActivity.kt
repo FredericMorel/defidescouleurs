be2b7bf22ed65afd08df149550de137a702eea6a
Frédéric Morel <frederic.morel.ars@outlook.fr>
2022-02-08 17:36:42 +0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/defidescouleurs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.defidescouleurs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              width: 100,
+              height: 1000,
+              color: Colors.red,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  color: Colors.yellow,
+                  width: 100.0,
+                  height: 100.0,
+                ),
+                Container(
+                  color: Colors.green,
+                  width: 100.0,
+                  height: 100.0,
+                ),
+              ],
+            ),
+            Container(
+              width: 100,
+              height: 1000,
+              color: Colors.blue,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
